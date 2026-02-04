924be739824a164a5ef0d13a5b8150ff2fba8444
asya.dishlieva@klevret.com <asya.dishlieva@klevret.com>
2026-02-04 22:01:46 +0200

    Add ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTracker.Models;
+
+namespace MovieTracker.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+    }
+}
