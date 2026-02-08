65fcafede6434e0ce7a1301262037664eb5450b2
asya.dishlieva@klevret.com <asya.dishlieva@klevret.com>
2026-02-08 18:52:34 +0200

    Add Genres page

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieTracker.Data;
+using MovieTracker.Models.Entities;
+
+namespace MovieTracker.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public GenresController(ApplicationDbContext dbcontext)
+        {
+            this._dbcontext = dbcontext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var genres = await _dbcontext.Genres.ToListAsync();
+            return View(genres);
+        }
+    }
+}
