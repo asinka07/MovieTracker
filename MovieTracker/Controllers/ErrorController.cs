21f3db4e58993bb0a79a78cc5f59af1f06a08372
asya.dishlieva@klevret.com <asya.dishlieva@klevret.com>
2026-04-06 22:45:30 +0300

    Add custom 404 and 500 error pages with error handling middleware

---
+++ MovieTracker/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieTracker.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult Index(int statusCode)
+        {
+            if (statusCode == 404)
+                return View("Error404");
+
+            return View("Error500");
+        }
+    }
+}
--- MovieTracker/Program.cs
+++ MovieTracker/Program.cs
@@ -22,16 +22,17 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IDirectorService, DirectorService>();
 builder.Services.AddScoped<IWatchlistService, WatchlistService>();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseHttpsRedirection();
 app.UseRouting();
